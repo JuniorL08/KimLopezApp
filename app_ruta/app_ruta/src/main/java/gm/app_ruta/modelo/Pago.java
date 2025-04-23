b0acb4ddf6f87d37185d12235949a359f6f3f50a
JuniorL08 <junior-18@hotmail.es>
2025-04-23 12:44:01 -0400

    Se aregaron las notaciones ToString and EqualsAndHashCode de JPA, y se dejo en una sola linea los atributos de tipo String

--- app_ruta/app_ruta/src/main/java/gm/app_ruta/modelo/Pago.java
+++ app_ruta/app_ruta/src/main/java/gm/app_ruta/modelo/Pago.java
@@ -4,20 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Pago {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String cobrador;
-    private String nombreCliente;
+    private String cobrador,nombreCliente, fecha;
     private Double montoPago;
-    private String fecha;
 }
