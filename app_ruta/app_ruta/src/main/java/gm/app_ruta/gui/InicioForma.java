07440c428aab88698ef7cd2905b687d6b59f908d
JuniorL08 <junior-18@hotmail.es>
2025-04-24 20:26:30 -0400

    Se creo la GUI del inicio principal como InicioForma

--- app_ruta/app_ruta/src/main/java/gm/app_ruta/gui/InicioForma.java
+++ app_ruta/app_ruta/src/main/java/gm/app_ruta/gui/InicioForma.java
@@ -1,4 +1,19 @@
 package gm.app_ruta.gui;
 
-public class InicioForma {
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+@Component
+public class InicioForma extends JFrame{
+    private JPanel panelPrincipal;
+    private JButton ARTICULOSButton;
+    private JButton CLIENTESButton;
+    private JButton VENTASButton;
+
+    private void iniciarForma(){
+        setContentPane(panelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900,700);
+        setLocation(null);
+    }
 }
